53ed48327ea85c179d8361ced257e3ab42b8c4f3
max-translia <32696645+max-translia@users.noreply.github.com>
2021-08-17 16:29:17 +0800

    init

---
+++ servlet/src/com/liang/servlet/MyServlet.java
@@ -0,0 +1,26 @@
+package com.liang.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class MyServlet  extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 向浏览器输出内容
+        // 设置编码
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().write("hello, 这是我的第一个Servlet...");
+        response.getWriter().write("当前系统时间是："+new Date());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
---
+++ servlet/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liang
+  Date: 2020-09-11
+  Time: 13:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  ahahahah
+  </body>
+</html>
